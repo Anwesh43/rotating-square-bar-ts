96d73b5a9c4e6e284d3322d577c4920d29e0e5e3
Anwesh43 <anweshthecool0@gmail.com>
2020-06-21 21:57:12 +0530

    addiing ScaleUtil class

--- index.ts
+++ index.ts
@@ -6,3 +6,18 @@ const barHFactor :number = 3.2
 const colors : Array<String> = ["#4CAF50", "#3F51B5", "#9C27B0", "#f44336", "#0091EA"]
 const backColor : String = "#BDBDBD"
 const delay : number = 20
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+
+    static sinify(scale : number) : number {
+        return Math.sin(scale * Math.PI)
+    }
+}
