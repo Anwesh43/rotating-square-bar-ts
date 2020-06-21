711b60cbf5230d16327b156f30ad3245d3486c9e
Anwesh43 <anweshthecool0@gmail.com>
2020-06-21 22:24:55 +0530

    adding RSBNode class

--- index.ts
+++ index.ts
@@ -131,3 +131,46 @@ class Animator {
         }
     }
 }
+
+
+class RSBNode {
+
+    state : State = new State()
+    prev : RSBNode = null
+    next : RSBNode = null
+
+    constructor(public i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RSBNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawRotatingSquareBar(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : RSBNode {
+        var curr : RSBNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr != null) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
